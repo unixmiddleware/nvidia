0e8f2bfd96c608dba17e91784432bc115ccb515a
Mark Pritchett <unixmiddleware@gmail.com>
2020-09-18 08:25:14 +0100

    Corrected typos

--- myIdentify-1.py
+++ myIdentify-1.py
@@ -8,8 +8,8 @@ disp=jetson.utils.glDisplay()
 font=jetson.utils.cudaFont()
 
 while disp.IsOpen():
-    frame,width,height=cam.CaptureRGBA
+    frame,width,height=cam.CaptureRGBA()
     classID,confident=net.Classify(frame,width,height)
-    item=net.GetClassDescription(classID)
+    item=net.GetClassDesc(classID)
     font.OverlayText(frame,width,height,item,5,5,font.Magenta,font.Blue)
     disp.RenderOnce(frame,width,height)
